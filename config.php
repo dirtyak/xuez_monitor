c842c1f09e8f4f410108d9f03a279d9880ba935d
dirtyak <supmarvin@gmail.com>
2018-04-18 20:58:04 +0000

    update

--- config.php
+++ config.php
@@ -1,18 +1,15 @@
 <?php
-
 ########## CHANGE THIS ##################################
 $rpc_user = "xuezrpc";
 $rpc_password = "Dt9Sz58ReHfAe7Ndg4pyp4rsTdXX8Hepn8Py1DDAckoV";
 
-##########  OPTIONNAL ##############################
-# (OPTION) CHANGE IF YOUR NODE IS REMOTE
+########## (OPTIONS) #################################
 $rpc_url = "localhost"; # Default : localhost
 $rpc_port = "41799"; # Default : 41799
-
-$refresh_delay = "2"; # refresh data every x seconds
+$refresh_delay = "10"; # refresh data every x seconds
 $date_format = 'm/d/Y H:i:s'; # How to format dates
 $xuez_explorer = "xuez.donkeypool.com"; # Optional
 
-##########   OPTIONNAL your masternodes addresses ##
+#### MORE MASTERNODES ADDRESSES ###
 $custom_mnlist = array("ADDR1", "ADDR2", "ADDR3");
 ?>
