2cea6167aecfc5f90299eeed74cabfa68e55bf68
dirtyak <supmarvin@gmail.com>
2018-04-18 20:53:51 +0000

    update

--- config.php
+++ config.php
@@ -1,20 +1,18 @@
 <?php
 
-# RPC DETAILS (MUST BE SAME AS XUEZ.CONF)
+########## CHANGE THIS ##################################
 $rpc_user = "xuezrpc";
 $rpc_password = "Dt9Sz58ReHfAe7Ndg4pyp4rsTdXX8Hepn8Py1DDAckoV";
 
-# RPC DETAILS (CHANGE IF XUEZ HOSTED REMOTELY)
-# MAKE SURE TO USE SAME CONFIG AS XUEZ.CONF
-$rpc_url = "localhost";
-$rpc_port = "41799";
+##########  OPTIONNAL ##############################
+# (OPTION) CHANGE IF YOUR NODE IS REMOTE
+$rpc_url = "localhost"; # Default : localhost
+$rpc_port = "41799"; # Default : 41799
 
-# CONFIG
 $refresh_delay = "2"; # refresh data every x seconds
 $date_format = 'm/d/Y H:i:s'; # How to format dates
 $xuez_explorer = "xuez.donkeypool.com"; # Optional
 
-# OPTIONNAL # your masternodes addresses
-# array("ADDR1", "ADDR2", "ADDR3");
-$custom_mnlist = array("", "", "");
+##########   OPTIONNAL your masternodes addresses ##
+$custom_mnlist = array("ADDR1", "ADDR2", "ADDR3");
 ?>
